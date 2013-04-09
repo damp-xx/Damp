5f4540775f52b706b07c708ce96a28fbd9e70306
Filip Vestergaard <11733@iha.dkk>
2013-04-09 10:14:29 +0200

    TankGame added

---
+++ TankGame/TankGame/TankGame/GameSystem.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using TankGame.Game_Screens;
+
+namespace TankGame
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class GameSystem : Microsoft.Xna.Framework.Game
+    {
+        
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        //Background 
+       /* private Texture2D background1Texture;
+        private Vector2 background1Position;
+        private Texture2D background2Texture;
+        private Vector2 background2Position;
+        private Texture2D background3Texture;
+        private Vector2 background3Position;
+        private Texture2D background4Texture;
+        private Vector2 background4Position;*/
+
+
+        private IMenu menuShown;
+
+        public GameSystem()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferHeight = 768;
+            graphics.PreferredBackBufferWidth = 1366;
+        
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            menuShown = new PlayMenu(GraphicsDevice.Viewport, Content);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Map
+           
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            menuShown.Update();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            menuShown.Draw(spriteBatch);
+                  
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TankGame/TankGame/TankGame/IMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TankGame
+{
+    interface IMap
+    {
+        List<Texture2D> TextureMap { get; }
+        List<Vector2> PostionMap { get; }
+
+        void Draw(SpriteBatch spriteBatch);
+
+        void LoadContent(ContentManager content);
+
+        void Initialize();
+    }
+}
