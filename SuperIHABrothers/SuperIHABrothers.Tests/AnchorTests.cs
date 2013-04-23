f239721e11b05953713bf78bf48281533f0484cf
Space-Punk <space-punk@hotmail.com>
2013-04-23 09:28:58 +0200

    Test project added

---
+++ SuperIHABrothers/SuperIHABrothers.Tests/AnchorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using Sprites;
+using SuperIHABrothers;
+namespace SuperIHABrothers.Tests
+{
+    [TestFixture]
+    public class AnchorTests
+    {
+        private Anchor _uut;
+        [SetUp]
+        public void Init()
+        {
+            _uut = new Anchor();
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void SetSpeed_InvalidInput_ExceptionThrown()
+        {
+            _uut.SetSpeed(-1);
+        }
+
+    }
+}
---
+++ SuperIHABrothers/SuperIHABrothers/SuperIHABrothers/GameState/IKeybordInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperIHABrothers.GameState
+{
+    
+
+    public interface IKeybordInput
+    {
+        bool IsLeftPressed { get; }
+    }
+}
