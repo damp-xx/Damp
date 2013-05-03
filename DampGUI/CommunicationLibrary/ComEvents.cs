4042bae932ff931ffd40479af594fc52fc32924d
Michael Jakobsen <10352@iha.dk>
2013-05-03 13:26:56 +0200

    Copy for implementation of gui and network

---
+++ DampGUI/CommunicationLibrary/ComEvents.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using CommunicationLibrary.Events;
+using ConnectionLibrary;
+using DampCS;
+
+namespace CommunicationLibrary
+{
+    public class ComEvents : IEventParser
+    {
+        public static void Listen()
+        {
+            var client = new DampServerClient(ComLogin._ComIp);
+
+            client.Listen(ComLogin._ComToken, new ComEvents());
+
+        }
+
+        public void ParseEvent(XmlElement Event)
+        {
+            IEvents command = null;
+            Console.WriteLine("Handle Element: {0}", Event.InnerXml);
+            if (Event.Name.Equals("Status"))
+            {
+                switch (Event.GetElementsByTagName("Command").Item(0).InnerText)
+                {
+                    case "UserWentOnline":
+                        command = new UserWentOnline();
+                        break;
+                    case "UserWentOffline":
+                        command = new UserWentOffline();
+                        break;
+                    case "FriendRequest":
+                        command = new FriendRequest();
+                        break;
+                    case "FriendAccepted":
+                        command = new FriendAccepted();
+                        break;
+                    case "ChatRecieved":
+                        command = new NewChatMessage();
+                        break;
+                    default:
+                        Console.WriteLine("Error parseing Event: {0}", Event.InnerXml);
+                        break;
+                }
+                try
+                {
+                    command.Action(Event);
+                }
+                catch (NullReferenceException NEx)
+                {
+                    Console.WriteLine("Event not recogniced ....");
+                }
+            }
+        }
+    }
+}
---
+++ DampGUI/CommunicationLibrary/Events/FriendRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CommunicationLibrary.Events
+{
+    class FriendRequest: IEvents
+    {
+        public void Action(XmlElement Event)
+        {
+            Console.WriteLine("Friendrequest from: {0} please accept or decline this", Event.GetElementsByTagName("From").Item(0).InnerText);
+        }
+    }
+}
