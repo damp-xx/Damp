adf94862349476576aa4dd08c8be6d849efda134
jenkins <11841@iha.dk>
2013-05-07 09:24:55 +0200

    added more attributes to user object

--- DampServer/DampServer/UserManagement.cs
+++ DampServer/DampServer/UserManagement.cs
@@ -36,7 +36,7 @@ namespace DampServer
             }
             else throw new UserNotFoundException();
 
-
+            /*
             cmd.CommandText =
                 "SELECT U.userid as userid, U.username as username FROM Users U INNER JOIN Friends F ON F.userid1 = U.userid WHERE F.userid = @userid";
             cmd.Parameters.Add("@userid", SqlDbType.BigInt).Value = user.UserId;
@@ -51,6 +51,7 @@ namespace DampServer
                     user.Friends.Add(new User {Username = (string) r["username"], UserId = (long) r["userid"]});
                 }
             }
+             */
 
             r.Close();
             db.Close();
