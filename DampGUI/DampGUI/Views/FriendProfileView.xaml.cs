3aa012b4f65c765d8b49481e20930e8c176b15ed
Magnus Pedersen <louvmand@gmail.com>
2013-05-09 22:30:02 +0200

    lidt mere

---
+++ DampGUI/DampGUI/Views/FriendProfileView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DampGUI
+{
+    /// <summary>
+    /// Interaction logic for FriendProfileView.xaml
+    /// </summary>
+    public partial class FriendProfileView : UserControl
+    {
+        public FriendProfileView()
+        {
+            InitializeComponent();
+        }
+    }
+}
