55212905c61aff44ca777be9596b341db6a536a1
Space-Punk <space-punk@hotmail.com>
2013-05-09 10:52:54 +0200

    Committed Old Super

---
+++ SuperIHABrothers/SuperIHABrothers/SuperIHABrothers/GameState/IKeybordInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameState
+{
+    
+
+    public interface IKeybordInput
+    {
+        bool IsLeftPressed { get; }
+        bool WasLeftPressed { get; }
+        bool IsRightPressed { get; }
+        bool WasRightPressed { get; }
+        bool IsJumpPressed { get; }
+        bool WasJumpPressed { get; }
+    }
+}
---
+++ SuperIHABrothers/SuperIHABrothers/SuperIHABrothers/Sprite/ISpriteContainerMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sprites;
+
+namespace SuperIHABrothers.Sprite
+{
+    public interface ISpriteContainerMessage
+    {
+        void AddSprite(ISprite sprite);
+    }
+}
