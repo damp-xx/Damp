a96fa68e3d71e9e89abd6a933080904320946b4e
Magnus Pedersen <louvmand@gmail.com>
2013-05-17 08:46:13 +0200

    lidt ekstra

---
+++ DampGUIv2/CommunicationLibrary/Models/PhotoCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DampGUI
+{
+    public class PhotoCollection : List<IPhoto>, IPhotoCollection
+    {
+        public PhotoCollection(List<string> listUrl)
+        {
+            IsMade = false;
+            foreach (var url in listUrl)
+            {
+                if (url != null)
+                    Add(new Photo(url));
+            }
+        }
+
+        public bool IsMade { get; set; }
+
+    }
+}
